1d81178e189b9ca6ae4a94fbc5428c68d98fc7b8
Zihua Liu <zliu@Zihuas-MacBook-Pro.local>
2016-07-15 23:20:30 -0700

    Implemented call to Google Maps API

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var express = require('express');
-// var http = require('http');
-// var server = http.createServer(app);
 var app = express();
 var path = require("path");
+var request = require("request")
 
 
+//static var for google maps api 
 var API_KEY = "key=AIzaSyCx6MMMEoT9dFT0okzS77vk80sNqLJIgjo"
 var MAP_API = "https://maps.googleapis.com/maps/api/distancematrix/"
 var RETURN_TYPE = "json"
@@ -13,30 +13,75 @@ var MODE = "mode=walking"
 var ORI = "origins="
 var DEST = "destinations="
 
-// MAP_API + RETURN_TYPE + ? +UNITS + & + MODE + & + ORI + & + DEST + API_KEY
+// MAP_API + RETURN_TYPE + ? +UNITS + & + MODE + & + ORI + & + DEST + & + API_KEY
 
+
+var requestString = "https://maps.googleapis.com/maps/api/distancematrix/json?untis=imperial&mode=walking&origins=37.427156,-122.080711&destinations=37.423370,-122.071474&key=AIzaSyCx6MMMEoT9dFT0okzS77vk80sNqLJIgjo"
 //landing point
 app.get('/', function (req, res) {
 	res.sendFile('index.html' , { root : path.join(__dirname, "../frontend")});
 });
 
-// template request on data
+// template request on geo data
 app.get("/data", function(req, res) {
-	var dummyJSON = {
-		"url": "http://imgur.com/gallery/0DQQTAv",
-		"location": {
+
+	// sample location data (acquired)
+	var myLocation = {
 			"lattitude":37.4233111,
-			"longtitude": 122.07064579999997 
+			"longtitude": -122.07064579999997 
+	};
+
+	// sample target locations (Insta API)
+	var interestList = [ 
+		{
+			"lattitude": 37.332144, 
+			"longtitude":-122.031234
 		},
-		"name": "LinkedIn"
-	}
-	var list = [];
-	for (i < 0; i < 10; i++) {
-		list.push(dummyJSON)
+		{
+			"lattitude": 37.426811, 
+			"longtitude":-122.078655
+		}
+	]
+
+	// query for every interest point =) 
+	var list = []
+	for (i = 0; i < interestList.length; i++) {
+
+		//generat request url 
+		var reqUrl = MAP_API + RETURN_TYPE + "?" +UNITS + "&" + MODE + "&" + ORI + myLocation["lattitude"]
+		+ "," + myLocation["longtitude"] + "&" + DEST + interestList[i]["lattitude"] + "," +  interestList[i]["longtitude"] + "&" + API_KEY
+		console.log(reqUrl);
+
+		//issue request to Google Maps API
+		request( {
+			url: reqUrl,
+			method: "GET"
+		}, function (error, response, body) {
+		  if (!error && response.statusCode == 200) {
+		  	// add into list of interest point distances
+		  	list.push(JSON.parse(body)["rows"][0]["elements"]);
+			console.log(list);
+		  }
+		});
+
 	}
 
-	// push data back?
+	res.sendFile('index.html' , { root : path.join(__dirname, "../frontend")});
+
+	//push data back
+	// yet to be implemented
+
+	//return type template
+	// var dummyJSON = [{
+	// 	"url": "http://imgur.com/gallery/0DQQTAv",
+	// 	"location": {
+	// 		"lattitude":37.4233111,
+	// 		"longtitude": 122.07064579999997 
+	// 	},
+	// 	"name": "LinkedIn"
+	// }]
 });
+
 
 // tempalte post request
 app.post('/choice', function(req, res) {	

e32a63208d5ecea75ef87c98a3f82be76c7c82de
David Zhu <hdavidzhu@gmail.com>
2016-08-02 19:29:15 -0700

    CLEAN.

--- server.js
+++ server.js
@@ -3,9 +3,6 @@ var path = require('path');
 var request = require('request');
 var config = require('./config.json');
 
-var app = express();
-
-//static var for google maps api
 var GOOGLE_MAPS_BASE_URL = "https://maps.googleapis.com/maps/api/distancematrix/";
 var RETURN_TYPE = "json"
 var UNITS = "units=imperial"
@@ -13,60 +10,62 @@ var MODE = "mode=walking"
 var ORI = "origins="
 var DEST = "destinations="
 
+var app = express();
+
 app.use('/', express.static('public'));
 
 var userData = [];
 
 app.get("/auth", function(req, res) {
-	userData = []
-	var clientCode = req.param("code");
+  userData = []
+  var requestConf = {
+    url: 'https://api.instagram.com/oauth/access_token',
+    method: 'POST',
+    form: {
+      "client_id":"a0cb68128abd4ef99d23451fe30657a6",
+      "client_secret":"7834c945f05c4a2baf4f96cb088e0cd6",
+      "grant_type":"authorization_code",
+      "redirect_uri":"http://10.16.20.247:8083/auth",
+      "code": req.param("code")
+    }
+  }
+  var requestCallback = function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      accessData = JSON.parse(body);
+      accessToken = accessData["access_token"];
 
-	console.log(clientCode)
-	var reqUrl = "https://api.instagram.com/oauth/access_token"
-	var reqBody = {
-		"client_id":"a0cb68128abd4ef99d23451fe30657a6",
-		"client_secret":"7834c945f05c4a2baf4f96cb088e0cd6",
-		"grant_type":"authorization_code",
-		"redirect_uri":"http://10.16.20.247:8083/auth",
-		"code": clientCode
-	}
-	request( {
-			url: reqUrl,
-			method: "POST",
-			form: reqBody
-		}, function (error, response, body) {
-		  if (!error && response.statusCode == 200) {
-		  	// console.log(body)
-		  	access_data = JSON.parse(body);
-		  	access_token = access_data["access_token"];
-		  	var finalURL = "https://api.instagram.com/v1/users/search?q=hypotheticaleatsinsf&access_token=" + access_token
-			request({
-					url : finalURL,
-					method: "GET"
-				}, function(error, response, body) {
-					followed_data = JSON.parse(body);
-					var id = followed_data["data"][0]["id"];
-					var onionURL ="https://api.instagram.com/v1/users/" + id + "/media/recent/?access_token=" + access_token;
-					request({
-						url: onionURL,
-						method:"GET"
-					}, function(error, response, body) {
-						raw_onion = JSON.parse(body);
-						raw_onion = raw_onion["data"];
-						for  (var post of raw_onion) {
-							console.log(post["location"]["name"]);
-							userData.push( {
-								"name": post["location"]["name"],
-								"latitude": post["location"]["latitude"],
-								"longitude": post["location"]["longitude"],
-								"image_url": post["images"]["standard_resolution"]["url"]
-							});
-						}
-					});
-				}
-			);
-		}
-	});
+      var requestConf = {
+        url: 'https://api.instagram.com/v1/users/search?q=hypotheticaleatsinsf&access_token=' + accessToken,
+        method: 'GET'
+      }
+
+      request(requestConf, function(error, response, body) {
+        followedData = JSON.parse(body);
+        var id = followedData.data[0].id;
+        var requestConf = {
+          url: 'https://api.instagram.com/v1/users/' + id + '/media/recent/?access_token=' + accessToken,
+          method: 'GET'
+        }
+        request(requestConf, function(error, response, body) {
+          rawOnion = JSON.parse(body);
+          rawOnion = rawOnion["data"];
+          for (var post of rawOnion) {
+            console.log(post["location"]["name"]);
+            userData.push({
+              name:      post.location.name,
+              latitude:  post.location.latitude,
+              longitude: post.location.longitude,
+              imageUrl:  post.images.standard_resolution.url
+            });
+          }
+        });
+      });
+    }
+  }
+
+  request(requestConf, requestCallback);
+
+  // Redirect the user to the search page.
   res.redirect("/#!/search");
 });
 
@@ -86,65 +85,65 @@ app.get("/insta", function(req, res) {
 
 // template request on geo data
 app.get("/data", function(req, res) {
-	// sample location data (acquired)
-	var myLocation = {
-			"latitude":37.423601,
-			"longitude": -122.070718
-	};
+  // sample location data (acquired)
+  var myLocation = {
+      "latitude":37.423601,
+      "longitude": -122.070718
+  };
 
-	// sample target locations (Insta API)
-	console.log(userData);
-	var interestList = userData
+  // sample target locations (Insta API)
+  console.log(userData);
+  var interestList = userData
 
-	// query for every interest point
-	var list = []
-	for (i = 0; i < interestList.length; i++) {
-		var corLocation = interestList[i];
-		(function(corLocation) {
-		//generate request url
-		var reqUrl = MAP_API + RETURN_TYPE
-			+ "?" + UNITS
-			+ "&" + MODE
-			+ "&" + ORI
-				+ myLocation.latitude + ","
-				+ myLocation.longitude
-			+ "&" + DEST
-				+ corLocation["latitude"] + ","
-				+ corLocation["longitude"]
-			+ "&" + API_KEY;
+  // query for every interest point
+  var list = []
+  for (i = 0; i < interestList.length; i++) {
+    var corLocation = interestList[i];
+    (function(corLocation) {
+    //generate request url
+    var reqUrl = MAP_API + RETURN_TYPE
+      + "?" + UNITS
+      + "&" + MODE
+      + "&" + ORI
+        + myLocation.latitude + ","
+        + myLocation.longitude
+      + "&" + DEST
+        + corLocation["latitude"] + ","
+        + corLocation["longitude"]
+      + "&" + config.GOOGLE_MAPS_API_KEY;
 
-			request( {
-				url: reqUrl,
-				method: "GET"
-			}, function (error, response, body) {
-			  if (!error && response.statusCode == 200) {
+      request( {
+        url: reqUrl,
+        method: "GET"
+      }, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
 
-			  	// add into list of interest point distances
-			  	data = JSON.parse(body);
-			  	data = {
-					"image_url": corLocation["image_url"],
-					"location": {
-						"latitude": corLocation["latitude"],
-						"longitude": corLocation['longitude']
-					},
-					"distance": data["rows"][0]["elements"][0]["distance"],
-					"duration": data["rows"][0]["elements"][0]["duration"],
-					"name": corLocation["name"],
-					"address": data["destination_addresses"][0]
-			  	};
-			  	list.push(data);
-			  }
-			});
+          // add into list of interest point distances
+          data = JSON.parse(body);
+          data = {
+          "image_url": corLocation["image_url"],
+          "location": {
+            "latitude": corLocation["latitude"],
+            "longitude": corLocation['longitude']
+          },
+          "distance": data["rows"][0]["elements"][0]["distance"],
+          "duration": data["rows"][0]["elements"][0]["duration"],
+          "name": corLocation["name"],
+          "address": data["destination_addresses"][0]
+          };
+          list.push(data);
+        }
+      });
 
-		})(corLocation);
-	}
-	// rank location in order
-	setTimeout(function() {
-		list.sort (function(js1, js2) {
-			return js1["duration"]["value"] - js2["duration"]["value"];
-		});
-		res.json(list);
-	}, 300);
+    })(corLocation);
+  }
+  // rank location in order
+  setTimeout(function() {
+    list.sort (function(js1, js2) {
+      return js1["duration"]["value"] - js2["duration"]["value"];
+    });
+    res.json(list);
+  }, 300);
 });
 
 var PORT = 8003
